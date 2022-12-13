d3b189f2e7157f437fd223a197455a209e52e13c
2130056 <a2130056@2130056ui-MacBookPro.local>
2022-12-13 15:31:44 +0900

    add db

--- dart_fss.py
+++ dart_fss.py
@@ -9,7 +9,7 @@ load_dotenv()
 crtfc_key = os.getenv('CRTFC_KEY')
 
 # 기업 코드 ex) [{'corp_code': '00126380', 'corp_name': '삼성전자', 'stock_code': '005930', 'modify_date': '20220509'}]
-def get_corp_code(name, match=True):
+def get_corp_code(name, match=None):
     global crtfc_key
     url = 'https://opendart.fss.or.kr/api/corpCode.xml'
     params = {'crtfc_key': crtfc_key}
@@ -20,14 +20,18 @@ def get_corp_code(name, match=True):
     xml_str = xmlfile_objs['CORPCODE.xml'].decode('utf-8')
     data_dict = xmltodict.parse(xml_str).get('result', {}).get('list')
     result = []
-    if match:
+
+    if match == True:
         for item in data_dict:
             if name == item['corp_name']:
                 result.append(item)
-    else:
+    elif match == False:
         for item in data_dict:
             if name in item['corp_name']:
                 result.append(item)
+    else:
+        for item in data_dict:
+            result.append(item)
     return result
 
 # fs_div = CFS:연결재무제표, OFS:재무제표, sj_div = BS:재무상태표, IS:손익계산서
@@ -48,6 +52,6 @@ def get_corp_data(corp_code, bsns_year, reprt_code, fs_div='OFS', sj_div='BS'):
                 result.append(item)
     return result
 
-corp_code = get_corp_code('삼성전자')[0]
+corp_code = get_corp_code('삼성전자', True)[0]
 corp_data = get_corp_data(corp_code['corp_code'], '2021', '11011')
 print(corp_data)
---
+++ db.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+conn = None
+cur = None
+
+def init_db():
+    global conn, cur
+    conn = sqlite3.connect("trade.db")
+    cur = conn.cursor()
+
+def close():
+    global conn
+    conn.close()
+
+def commit():
+    global conn
+    conn.commit()
+
+def drop_table():
+    global cur
+    cur.execute("DROP TABLE trade_data")
+
+def create_table():
+    conn.execute('CREATE TABLE trade_data(corp_code TEXT, date TEXT)')
+    conn.execute('CREATE INDEX trade_data_idx1 ON trade_data(corp_code, date)')
+    
+def insert(corp_code, date):
+    global cur
+    cur.executemany(
+        'INSERT INTO trade_data VALUES (?, ?)',
+        [(corp_code, date)]
+    )
+
+def delete(corp_code, date):
+    global cur
+    cur.executemany(
+        'DELETE FROM trade_data WHERE corp_code = ? AND date = ?',
+        [(corp_code, date)]
+    )
+
+def show(corp_code, date):
+    global cur
+    sql = "SELECT * FROM trade_data WHERE corp_code = '{0}' AND date = '{1}'"
+    cur.execute(sql.format(corp_code, date))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
