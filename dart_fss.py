245d6b09d90e4f940230e719d385af26cb053e10
2130056 <a2130056@2130056ui-MacBookPro.local>
2023-01-06 11:36:31 +0900

    temp commit

--- dart_fss.py
+++ dart_fss.py
@@ -213,6 +213,7 @@ def get_custom_data(corp_code):
                                 unit_info[i]['str'] = unit
                                 break
 
+            # 단위 못찾을 경우 탐색
             if unit_info[i]['str'] is None:
                 df = get_df_data(fs_data[i])
                 origin = get_row_value(df,row_name='매출액')
@@ -237,23 +238,29 @@ def get_custom_data(corp_code):
                                                     break
 
                             if temp_unit_num is not None:
-                                temp_unit_str = None
-                                temp_len = len(str(temp_unit_num))
-                                origin_len = len(origin)
-                                comp_len = len(str(int(comp)*temp_unit_num))
-                                len_gap = comp_len - origin_len
-                                # print(origin, comp, temp_unit_num)
-                                if len_gap == 6 or temp_len + len_gap == 6:
-                                    temp_unit_str = '백만원'
-                                elif len_gap == 3 or temp_len + len_gap == 3:
-                                    temp_unit_str = '천원'
-                                elif len_gap == 0 or temp_len + len_gap == 0:
-                                    temp_unit_str = '원'
+                                cut_len = None
+                                if len(origin) < len(comp):
+                                    cut_len = len(origin)
+                                else:
+                                    cut_len = len(comp)
+                                if round(int(origin[0:cut_len-1])/int(comp[0:cut_len-1])) == 1:
+                                    temp_unit_str = None
+                                    origin_len = len(origin)
+                                    comp_len = len(str(int(comp)*temp_unit_num))
+                                    temp_len = len(str(temp_unit_num))
+                                    len_gap = comp_len - origin_len
+                                    # print(origin, comp, temp_unit_num)
+                                    if len_gap == 6 or temp_len + len_gap == 6:
+                                        temp_unit_str = '백만원'
+                                    elif len_gap == 3 or temp_len + len_gap == 3:
+                                        temp_unit_str = '천원'
+                                    elif len_gap == 0 or temp_len + len_gap == 0:
+                                        temp_unit_str = '원'
 
-                                unit_info[i]['str'] = {
-                                    'name': 'p',
-                                    'data': BeautifulSoup('<p>(단위:'+temp_unit_str+')</p>', 'html.parser')
-                                    }
+                                    unit_info[i]['str'] = {
+                                        'name': 'p',
+                                        'data': BeautifulSoup('<p>(단위:'+temp_unit_str+')</p>', 'html.parser')
+                                        }
         # units_num = []
         for i in range(len(unit_info)):
             unit_info[i]['num'] = None
@@ -301,11 +308,11 @@ def get_custom_data(corp_code):
 
 def insert_data():
     global all_data, row_name_list
-    corp_list = get_corp_code()
+    # corp_list = get_corp_code()
     # corp_list = [{'corp_code': '00956028', 'corp_name': '엑세스바이오', 'stock_code': '950130', 'modify_date': '20170630'}]
     # corp_list = [{'corp_code': '00232317', 'corp_name': '지오엠씨', 'stock_code': '033030', 'modify_date': '20170630'}]
     # corp_list = [{'corp_code': '01170962', 'corp_name': 'GRT', 'stock_code': '900290', 'modify_date': '20181122'}]
-    # corp_list = [{'corp_code': '00141389', 'corp_name': '영풍정밀', 'stock_code': '036560', 'modify_date': '20211208'}]
+    corp_list = [{'corp_code': '00141389', 'corp_name': '영풍정밀', 'stock_code': '036560', 'modify_date': '20211208'}]
     cnt = 0
     for corp_info in corp_list:
         print(corp_info)
